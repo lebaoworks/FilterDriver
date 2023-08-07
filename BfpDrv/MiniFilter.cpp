67afa1ab063097b75556532f843199220ba39715
lebaoworks <48866496+lebaoworks@users.noreply.github.com>
2023-08-08 01:47:54 +0700

    move utils to mini filter

--- BfpDrv/MiniFilter.cpp
+++ BfpDrv/MiniFilter.cpp
@@ -437,3 +437,50 @@ namespace MiniFilter
         return true;
     }
 }
+
+// Utils
+namespace MiniFilter::Utils
+{
+    _IRQL_requires_(PASSIVE_LEVEL)
+    NTSTATUS GetFileId(
+        _In_ PFLT_INSTANCE Instance,
+        _In_ PFILE_OBJECT FileObject,
+        _Out_ FILE_ID& FileId)
+    {
+        FileId = {};
+        FLT_FILESYSTEM_TYPE type;
+        auto status = FltGetFileSystemType(Instance, &type);
+        if (status != STATUS_SUCCESS)
+            return status;
+        if (type == FLT_FSTYPE_REFS)
+        {
+            FILE_ID_INFORMATION fileIdInformation;
+            status = FltQueryInformationFile(
+                Instance,                           // Instance
+                FileObject,                         // FileObject
+                &fileIdInformation,                 // FileInformation
+                sizeof(FILE_ID_INFORMATION),        // Length
+                FileIdInformation,                  // FileInformationClass
+                NULL);                              // ReturnLength
+            if (status == STATUS_SUCCESS)
+                RtlCopyMemory(&FileId.FileId128, &fileIdInformation.FileId, sizeof(FileId.FileId128));
+        }
+        else
+        {
+            FILE_INTERNAL_INFORMATION fileInternalInformation;
+            status = FltQueryInformationFile(
+                Instance,                           // Instance
+                FileObject,                         // FileObject
+                &fileInternalInformation,           // FileInformation
+                sizeof(FILE_INTERNAL_INFORMATION),  // Length
+                FileInternalInformation,            // FileInformationClass
+                NULL);                              // ReturnLength
+            if (status == STATUS_SUCCESS)
+            {
+                FileId.FileId64.Value = fileInternalInformation.IndexNumber.QuadPart;
+                FileId.FileId64.Reserved = 0LL;
+            }
+        }
+        return status;
+    }
+}
--- BfpDrv/MiniFilter.h
+++ BfpDrv/MiniFilter.h
@@ -83,3 +83,19 @@ namespace MiniFilter
         bool Authenticate(_In_ const Communication::Credential& Credential) const noexcept;
     };
 }
+
+namespace MiniFilter::Utils
+{
+    union FILE_ID
+    {
+        struct
+        {
+            UINT64 Value;
+            UINT64 Reserved;
+        } FileId64;
+        FILE_ID_128 FileId128;
+    };
+
+    // Get the file ID for the given file object. Does not work in PRE_CREATE callback.
+    _IRQL_requires_(PASSIVE_LEVEL) NTSTATUS GetFileId(_In_ PFLT_INSTANCE Instance, _In_ PFILE_OBJECT FileObject, _Out_ FILE_ID& FileId);
+}
